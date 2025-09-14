f228823b2f066d4a2fa08ff3f936082955d34f20
AlexMobiCraft <alex.mobicraft@gmail.com>
2025-09-14 21:22:01 +0200

    ci(workflow): исправьте подключение к базе данных для тестов
    Конвейер CI не мог подключиться к контейнеру сервиса PostgreSQL. В GitHub Actions сервисы открываются по адресу `localhost`, а не по имени хоста конкретного сервиса, как это принято в docker-compose.
    
    Этот коммит обновляет рабочий процесс, чтобы использовать `localhost` в качестве `DB_HOST`. Сценарий wait-for-postgres также настроен на проверку наличия базы данных на `localhost`. Кроме того, в настройках Django установлен пользователь по умолчанию для согласования с окружением CI.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
+        "USER": os.environ.get("DB_USER", "runner"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
